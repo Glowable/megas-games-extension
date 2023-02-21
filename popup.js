4b7ee3d228d506d20eff2e7b4c7ddcf92f7d16c4
Glow <leovonandion@gmail.com>
2023-02-21 02:03:28 +0100

    new styling

--- popup.js
+++ popup.js
@@ -6,24 +6,33 @@ document.addEventListener("DOMContentLoaded", function() {
   var websiteButton = document.getElementById("website");
   var forumButton = document.getElementById("forum");
 
-  usServerButton.addEventListener("click", function() {
-    chrome.tabs.create({url: "https://cfx.re/join/45yo89"});
-    window.close(); // close the extension window
-  });
+  if (usServerButton) {
+    usServerButton.addEventListener("click", function() {
+      chrome.tabs.create({url: "https://cfx.re/join/45yo89"});
+      window.close(); // close the extension window
+    });
+  }
 
-  euServerButton.addEventListener("click", function() {
-    chrome.tabs.create({url: "https://www.megasgames.com"});
-    window.close(); // close the extension window
-  });
+  if (euServerButton) {
+    euServerButton.addEventListener("click", function() {
+      chrome.tabs.create({url: "https://www.megasgames.com"});
+      window.close(); // close the extension window
+    });
+  }
 
-  websiteButton.addEventListener("click", function() {
-    chrome.tabs.create({url: "https://www.megasgames.com"});
-    window.close(); // close the extension window
-  });
+  if (websiteButton) {
+    websiteButton.addEventListener("click", function() {
+      chrome.tabs.create({url: "https://www.megasgames.com"});
+      window.close(); // close the extension window
+    });
+  }
 
-  forumButton.addEventListener("click", function() {
-    chrome.tabs.create({url: "https://forums.megasgames.com"});
-    window.close(); // close the extension window
-  });
+  if (forumButton) {
+    forumButton.addEventListener("click", function() {
+      chrome.tabs.create({url: "https://forums.megasgames.com"});
+      window.close(); // close the extension window
+    });
+  }
 });
+
 
